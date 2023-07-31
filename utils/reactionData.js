b13ec50d8292bcef1632ece4859b75e3b1ea6004
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-07-31 12:01:10 -0400

    added dummy reaction data

---
+++ utils/reactionData.js
@@ -0,0 +1,14 @@
+module.exports = reaction = [
+    {
+        reaction: 'this was great',
+        username: 'sagenthave'
+    },
+    {
+        reaction: 'good job',
+        username: 'natasha'
+    },
+    {
+        reaction: 'amazing',
+        username: 'millie'
+    }
+]
