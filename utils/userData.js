2ac2436eda2ccf714055a98ae6d92ad48aad0f92
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-07-31 11:58:38 -0400

    added dummy userData

---
+++ utils/userData.js
@@ -0,0 +1,14 @@
+module.exports = user = [
+    {
+        username: 'sagenthave',
+        email:'sagenthave@gmail.com'
+    },
+    {
+        username: 'natasha',
+        email: 'natasha@gmail.com'
+    },
+    {
+        username: 'millie',
+        email: 'millie@gmail.com'
+    }
+]
